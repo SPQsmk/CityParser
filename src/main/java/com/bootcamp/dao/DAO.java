a0b00840171e1e8989d740d1440d7e1f64ec0edb
SPQsmk <tver.asus@gmail.com>
2021-10-05 17:54:34 +0300

    Add database setter/getter

--- src/main/java/com/bootcamp/dao/DAO.java
+++ src/main/java/com/bootcamp/dao/DAO.java
@@ -1,5 +1,47 @@
 package com.bootcamp.dao;
 
-public class DAO {
+import com.bootcamp.models.City;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
+import javax.persistence.PersistenceException;
+import java.util.List;
+
+public class DAO {
+    SessionFactory factory;
+
+    public DAO() {
+        try {
+            Configuration configuration = new Configuration().configure();
+            configuration.addAnnotatedClass(City.class);
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+            factory = configuration.buildSessionFactory(builder.build());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    public void writeCitiesToDB(List<City> list) {
+        try {
+            Session session = factory.openSession();
+            Transaction transaction = session.beginTransaction();
+
+            for (City o : list)
+                session.save(o);
+
+            transaction.commit();
+            session.close();
+        } catch (PersistenceException e) {
+            System.out.println("БД заполнена");
+        }
+    }
+
+    public List<City> getCitiesFromDB() {
+        Session session = factory.openSession();
+        return session.createQuery("select c from City c", City.class).getResultList();
+    }
 }
